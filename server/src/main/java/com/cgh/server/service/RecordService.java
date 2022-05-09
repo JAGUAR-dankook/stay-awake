c998c7df16e18e59ceb1ea6277c0c30935efd55d
ghchoi0427 <ghchoi0427@gmail.com>
2022-05-09 17:09:00 +0900

    feat: 날짜 and 사용자로 레코드 검색

--- server/src/main/java/com/cgh/server/service/RecordService.java
+++ server/src/main/java/com/cgh/server/service/RecordService.java
@@ -1,11 +1,13 @@
 package com.cgh.server.service;
 
+import com.cgh.server.domain.Member;
 import com.cgh.server.domain.Record;
 import com.cgh.server.repository.RecordRepository;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RecordService {
@@ -16,7 +18,15 @@ public class RecordService {
         this.recordRepository = recordRepository;
     }
 
-    public List<Record> findRecordsByStartBetween(LocalDateTime from, LocalDateTime to) {
-        return recordRepository.findRecordsByStartBetween(from, to);
+    public List<Record> findRecordsByMember(Member member) {
+        return recordRepository.findRecordsByMember(member);
+    }
+
+    public void save(Record record) {
+        recordRepository.save(record);
+    }
+
+    public Optional<Record> findRecordByStartDateAndMember(LocalDate startDate, Member member) {
+        return recordRepository.findRecordByStartDateAndMember(startDate, member);
     }
 }
