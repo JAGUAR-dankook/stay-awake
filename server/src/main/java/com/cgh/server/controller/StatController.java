603f7aced80c39365effc9d7744b37cf0232eb4f
ghchoi0427 <ghchoi0427@gmail.com>
2022-05-28 21:12:35 +0900

    fix: TimeFormatter 사용

--- server/src/main/java/com/cgh/server/controller/StatController.java
+++ server/src/main/java/com/cgh/server/controller/StatController.java
@@ -4,6 +4,7 @@ import com.cgh.server.domain.Member;
 import com.cgh.server.domain.Record;
 import com.cgh.server.service.MemberService;
 import com.cgh.server.service.RecordService;
+import com.cgh.server.util.TimeFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
@@ -37,28 +38,16 @@ public class StatController {
         String weekRecord = NO_RECORD;
 
         if (recordService.findRecordByStartDateAndMember(LocalDate.now(), member).isPresent()) {
-            dayRecord = timeFormatter(recordService.findRecordByStartDateAndMember(LocalDate.now(), member).get().getSeconds());
+            dayRecord = TimeFormatter.format(recordService.findRecordByStartDateAndMember(LocalDate.now(), member).get().getSeconds());
         }
         if (!recordService.findRecordsByStartDateBetweenAndMember(now.with(fieldISO, 1), now, member).isEmpty()) {
-            weekRecord = timeFormatter(sumRecords(recordService.findRecordsByStartDateBetweenAndMember(now.with(fieldISO, 1), now, member)));
+            weekRecord = TimeFormatter.format(sumRecords(recordService.findRecordsByStartDateBetweenAndMember(now.with(fieldISO, 1), now, member)));
         }
 
         model.addAttribute("dayrecord", dayRecord);
         model.addAttribute("weekrecord", weekRecord);
 
         return "status";
-    }
-
-    private String timeFormatter(int seconds) {
-        int sec = seconds % 60;
-        int min = (seconds / 60) % 60;
-        int hours = (seconds / 60) / 60;
-
-        String strSec = (sec < 10) ? "0" + sec : Integer.toString(sec);
-        String strMin = (min < 10) ? "0" + min : Integer.toString(min);
-        String strHours = (hours < 10) ? "0" + hours : Integer.toString(hours);
-
-        return strHours + ":" + strMin + ":" + strSec;
     }
 
     private int sumRecords(List<Record> records) {
